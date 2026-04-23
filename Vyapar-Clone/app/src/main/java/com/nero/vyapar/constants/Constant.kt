7d6f4fd33faf98e2d454198e6067d16c6634c9a6
Raulji-Ranpriyasinh <ranpriyasinhraulji@gmail.com>
2026-04-23 12:42:35 +0530

    add

---
+++ Vyapar-Clone/app/src/main/java/com/nero/vyapar/constants/Constant.kt
@@ -0,0 +1,16 @@
+package com.nero.vyapar.constants
+
+class Constants {
+
+    companion object {
+        const val PURCHASE = "purchase"
+        const val SALE = "sale"
+        const val SALEINT = 6
+        const val YOUWILLGETINT = 7
+        const val YOUWILLGIVEINT = 8
+        const val PURCHASEINT = 9
+        const val PRODUCT = "product"
+        const val SERVICE = "service"
+    }
+
+}
---
+++ Vyapar-Clone/app/src/main/java/com/nero/vyapar/local/database/VyaparDatabase.kt
@@ -0,0 +1,17 @@
+package com.nero.vyapar.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.nero.vyapar.local.dao.VyaparDAO
+import com.nero.vyapar.local.entity.ExpenseEntity
+import com.nero.vyapar.local.entity.ItemsEntity
+import com.nero.vyapar.local.entity.PartyEntity
+import com.nero.vyapar.local.entity.TransactionEntity
+
+@Database(
+    entities = [ItemsEntity::class, PartyEntity::class, TransactionEntity::class, ExpenseEntity::class],
+    version = 1
+)
+abstract class VyaparDatabase : RoomDatabase() {
+    abstract fun getDAO(): VyaparDAO
+}
